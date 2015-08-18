e5551d8ae7478bfe724d0a5cf50cfaf316738d6b
yehaido <yehaido@yeah.net>
2015-08-18 21:17:30 +0800

    Database utility

---
+++ src/com/coolweather/app/util/Utility.java
@@ -0,0 +1,13 @@
+package com.coolweather.app.util;
+
+import com.coolweather.app.db.CoolWeatherDB;
+
+
+public class Utility {
+
+	public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB,
+			String response){
+		return false;
+	}
+
+}
